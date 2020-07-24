00252ee7eb33873d2e34336b547b7169e2391879
kokche <kokche12@gmail.com>
2020-07-24 15:12:09 +0300

    Init commit

---
+++ app/src/main/java/com/example/nytimes/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.nytimes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nytimes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
