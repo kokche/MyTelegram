26711addf24856c817ebfd5b7d7fd7f03264c65b
kokche <kokche12@gmail.com>
2020-07-24 18:04:41 +0300

    Add Material Drawer

--- app/src/main/java/com/example/nytimes/MainActivity.kt
+++ app/src/main/java/com/example/nytimes/MainActivity.kt
@@ -1,13 +1,70 @@
 package com.example.nytimes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.nytimes.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mToolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunck()
+    }
+
+    private fun initFunck() {
+        setSupportActionBar(mToolbar);
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        mDrawer = DrawerBuilder()
+            .withAccountHeader(mHeader)
+            .withActivity(this)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withToolbar(mToolbar)
+            .addDrawerItems(
+                PrimaryDrawerItem()
+                    .withIconTintingEnabled(true)
+                    .withIdentifier(100)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+            ).build()
+    }
+
+    private fun createHeader() {
+        mHeader = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem()
+                    .withName("")
+                    .withEmail("")
+            ).build()
+    }
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
     }
 }
